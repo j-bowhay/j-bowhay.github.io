2931188978bba3dbb8111e2b50d2fe1dcec6ec4c
Jake Bowhay <jb9.bowhay@gmail.com>
2025-03-30 23:01:08 +0100

    add map page

---
+++ _pages/misc.md
@@ -0,0 +1,10 @@
+---
+layout: page
+title: misc
+nav: true
+nav_order: 8
+dropdown: true
+children:
+  - title: Countries Visited
+    permalink: /assets/visited_places/index.html
+---
---
+++ assets/visited_places/countries.js
@@ -0,0 +1,28 @@
+// Use ISO alpha-3 country codes, see e.g. https://en.wikipedia.org/wiki/ISO_3166-1_alpha-3
+COUNTRIES =
+{
+    AUT: {fillKey: 'visited'},
+    BEL: {fillKey: 'visited'},
+    BLZ: {fillKey: 'visited'},
+    CHN: {fillKey: 'visited'},
+    CRI: {fillKey: 'visited'},
+    CYP: {fillKey: 'visited'},
+    SLV: {fillKey: 'visited'},
+    FRA: {fillKey: 'visited'},
+    DEU: {fillKey: 'visited'},
+    GRC: {fillKey: 'visited'},
+    GTM: {fillKey: 'visited'},
+    HND: {fillKey: 'visited'},
+    IRL: {fillKey: 'visited'},
+    GBR: {fillKey: 'visited'},
+    ITA: {fillKey: 'visited'},
+    JEY: {fillKey: 'visited'},
+    MEX: {fillKey: 'visited'},
+    NLD: {fillKey: 'visited'},
+    NIC: {fillKey: 'visited'},
+    NOR: {fillKey: 'visited'},
+    PAN: {fillKey: 'visited'},
+    POL: {fillKey: 'visited'},
+    ESP: {fillKey: 'visited'},
+    USA: {fillKey: 'visited'},
+}

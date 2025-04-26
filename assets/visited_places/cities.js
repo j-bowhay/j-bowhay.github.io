8d54d372f269ebd7f2c4e8b97bda2c4eedf4c46f
Jake Bowhay <jb9.bowhay@gmail.com>
2025-04-26 18:21:27 +0100

    add Edinburgh

--- assets/visited_places/cities.js
+++ assets/visited_places/cities.js
@@ -3,4 +3,5 @@ CITIES = [
   { name: "University of Oxford", latitude: 51.755, longitude: -1.255, radius: 4, fillKey: "city" },
   { name: "University of Washington", latitude: 47.654167, longitude: -122.308056, radius: 4, fillKey: "city" },
   { name: "Southwest Jiaotong University", latitude: 30.769444, longitude: 103.984722, radius: 4, fillKey: "city" },
+  { name: "University of Edinburgh", latitude: 55.95, longitude: -3.183333, radius: 4, fillKey: "city" },
 ];
